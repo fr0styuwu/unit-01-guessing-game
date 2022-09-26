1a752fd044854803706b0e6423d8bd7958860c92
fr0styuwu <111027135+fr0styuwu@users.noreply.github.com>
2022-09-26 04:23:26 -0500

    Add files via upload

---
+++ unit-01/game.py
@@ -0,0 +1,119 @@
+# imports the necessary module
+import random
+
+highscore_e = 10
+highscore_m = 10
+highscore_h = 10
+
+# defines the name of the user and welcomes them to the game
+name = input('Please tell me your name: ' ).capitalize()
+print('Welcome',name,'to my random number guessing game!')
+
+# defines the main function of the code
+def start_game(): 
+    # sets how many guesses it takes for the user to get the answer
+    guesses_e = 0
+    guesses_m = 0
+    guesses_h = 0
+
+    # sets the highscore for each difficulty
+    global highscore_e
+    global highscore_m
+    global highscore_h
+    
+    
+    # defines the difficulty the user wishes to tackle
+    difficulty = input('Please choose your desired difficulty between Easy, Medium, Hard: ').lower()
+    
+    # defines the function if the difficulty chosen is easy
+    if difficulty in ('easy', 'medium', 'hard'):
+        while difficulty == 'easy':
+            try:
+                number = random.randrange(1,10)
+                guess = int(input('Enter a number between 1 and 10: '))  
+                if guesses_e == 0:
+                    guesses_e += 1
+                while guess != number:
+                    if guess < number:
+                        print('Your guess was too low')
+                        guess = int(input('Enter a number between 1 and 10: '))
+                        guesses_e = guesses_e + 1
+                    elif guess > number:
+                        print('Your guess was too high.')
+                        guess = int(input('Enter a number between 1 and 10: '))
+                        guesses_e = guesses_e + 1
+                print("You got it! It took you {} tries.".format(guesses_e))
+                if guesses_e < highscore_e:
+                    highscore_e = guesses_e
+                break
+            except ValueError:
+                print('Please guess a number, thank you.') 
+            
+        # defines the function if the difficulty chosen is medium
+        while difficulty == 'medium':
+            try:
+                number = random.randrange(1,50)
+                guess = int(input('Enter a number between 1 and 50: '))  
+                if guesses_m == 0:
+                    guesses_m += 1
+                while guess != number:
+                    if guess < number:
+                        print('Your guess was too low')
+                        guess = int(input('Enter a number between 1 and 50: '))
+                        guesses_m = guesses_m + 1
+                    elif guess > number:
+                        print('Your guess was too high.')
+                        guess = int(input('Enter a number between 1 and 50: '))
+                        guesses_m = guesses_m + 1
+                print("You got it! It took you {} tries.".format(guesses_m))
+                if guesses_m < highscore_m:
+                    highscore_m = guesses_m
+                break
+            except ValueError:
+                print('Please guess a number, thank you.') 
+    
+            
+    
+        # defines the function if the difficulty chosen is hard
+        while difficulty == 'hard':
+            try:
+                number = random.randrange(1,100)
+                guess = int(input('Enter a number between 1 and 100: '))  
+                if guesses_h == 0:
+                    guesses_h += 1
+                while guess != number:
+                    if guess < number:
+                        print('Your guess was too low')
+                        guess = int(input('Enter a number between 1 and 100: '))
+                        guesses_h = guesses_h + 1
+                    elif guess > number:
+                        print('Your guess was too high.')
+                        guess = int(input('Enter a number between 1 and 100: '))
+                        guesses_h = guesses_h + 1
+                print("You got it! It took you {} tries.".format(guesses_h))
+                if guesses_h < highscore_h:
+                    highscore_h = guesses_h
+                break
+            except ValueError:
+                print('Please guess a number, thank you.') 
+    
+        new_game = input("Would you like to play again? yes or no: ").lower()
+        
+
+        
+
+        if new_game.lower() == 'yes':
+            print('HIGHSCORE\n Easy:',(highscore_e),
+                              'Normal:',(highscore_m),
+                              'Hard:',(highscore_h),
+                 'NOTE: The score is tallied by the least amount of tries it took to find the answer with the base score starting off at 10. The lower the number, the better you did. Aim for score of 1!')
+            start_game()
+    else:
+        print('Please choose a difficulty from easy, medium, or hard. Thank you.')
+        exit()
+    # if the user inputs anything other than easy, normal, or hard it will prompt the user to try again and end the function
+    print('Try again.')
+    
+
+if __name__ == '__start_game__':
+    start_game()
